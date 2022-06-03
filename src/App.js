5dedac3254044077afae15e23806d65457a47975
Eric Lim <jhlim0110@gmail.com>
2022-06-02 18:47:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Amplify, { apple, leet } from "./myExports";
+export default function App() {
+  console.log(apple);
+  console.log(Amplify);
+  console.log(Amplify());
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/myExports.js
@@ -0,0 +1,8 @@
+export const apple = "a2pple";
+export const leet = 1337;
+
+function iCanBeCalledAnything() {
+  return "Ampli9fy";
+}
+
+export default iCanBeCalledAnything;
